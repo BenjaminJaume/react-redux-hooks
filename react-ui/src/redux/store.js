318e2e02c2b810191d46d644c28a595cf7b58005
Benjamin Jaume <47286801+BenjaminJaume@users.noreply.github.com>
2021-07-08 17:59:07 -0600

    Fixing dependency bug

--- react-ui/src/redux/store.js
+++ react-ui/src/redux/store.js
@@ -2,14 +2,19 @@ import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from "./rootReducer";
+let store = {};
 
 // import logger from "redux-logger";
 // const middleware = [logger, thunk];
 const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(...middleware))
-);
+if (process.env.NODE_ENV === "development") {
+  store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(...middleware))
+  );
+} else {
+  store = createStore(rootReducer, applyMiddleware(...middleware));
+}
 
 export default store;
